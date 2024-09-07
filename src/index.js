22db822022309c31eabe91a71870a774e9fd875e
gowtham351999 <mathwogmathwog@gmail.com>
2024-09-07 22:39:40 +0530

    sentry added

--- src/index.js
+++ src/index.js
@@ -1,3 +1,4 @@
+import * as Sentry from "@sentry/react";
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
@@ -7,12 +8,27 @@ import { Provider } from 'react-redux';
 import { store } from './redux/store';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
+
+Sentry.init({
+  dsn: "https://3ee8fa9e2cb7800dd5ac105cf86642f8@o4507912636923904.ingest.de.sentry.io/4507912641314896",
+  integrations: [
+    Sentry.browserTracingIntegration(),
+    Sentry.replayIntegration(),
+  ],
+  // Tracing
+  tracesSampleRate: 1.0, //  Capture 100% of the transactions
+  // Set 'tracePropagationTargets' to control for which URLs distributed tracing should be enabled
+  tracePropagationTargets: ["localhost", /^https:\/\/yourserver\.io\/api/],
+  // Session Replay
+  replaysSessionSampleRate: 0.1, // This sets the sample rate at 10%. You may want to change it to 100% while in development and then sample at a lower rate in production.
+  replaysOnErrorSampleRate: 1.0, // If you're not already sampling the entire session, change the sample rate to 100% when sampling sessions where errors occur.
+});
+
 root.render(
   <React.StrictMode>
     <Provider store={store}>
     <App />
     </Provider>
-    
   </React.StrictMode>
 );
 
